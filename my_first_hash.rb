8ae18ff8f6cb8f2540c6e5acdaacaaca10ef830b
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-01 23:37:06 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -11,3 +11,18 @@ end
 def retrieval
   shipping_manifest["oil paintings"]
 end
+
+
+def adding
+  shipping_manifest = {
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
+                  "oil paintings" => 3
+                 }
+shipping_manifest["muskets"] = 2
+
+shipping_manifest["gun powder"] = 4
+
+return shipping_manifest
+end
+
