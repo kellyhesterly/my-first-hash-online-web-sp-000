9b4fccb4fe7fe7b015d96e35e8087dc55274f146
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-01 23:18:03 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -3,8 +3,7 @@ def my_hash
 end
 
 def shipping_manifest
-  {"whale bone corsets" => 5
-"porcelain vases" => 2
-"oil paintings" => 3}
- end
+  {"whale bone corsets" => 5,
+   "porcelain vases" => 2,
+   "oil paintings" => 3}
 end
