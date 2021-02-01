fd658398637f91a90eb062565a0a2e40843e713d
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-01 23:26:07 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,18 +1,1 @@
-def my_hash
-  {"key" => "value"}
-end
 
-def shipping_manifest
-  {"whale bone corsets" => 5,
-   "porcelain vases" => 2,
-   "oil paintings" => 3}
-end
-
-def retrieval
-  shipping_manifest["oil paintings"]
-end
-
-def adding
-  shipping_manifest["gun powder"] = 4
-  return shipping_manifest
-end
