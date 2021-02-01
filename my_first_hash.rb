88f51f8d2b6727305103311bd9b4bfcfce2b75d9
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-01 23:31:50 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -18,5 +18,5 @@ def adding
   shipping_manifest["oil paintings"] = 3
   shipping_manifest["muskets"] = 2
   shipping_manifest["gun powder"] = 4
-  shipping_manifest
+  return shipping_manifest
 end
